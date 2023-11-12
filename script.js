373b6bd8416d3684e9e31fe98cf797dcbbae3985
Arsh <pawar.arsh1333@gmail.com>
2023-11-13 02:48:46 +0530

    runCode

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const css_code = document.querySelector(".css-code textarea");
 const js_code = document.querySelector(".js-code textarea");
 const result = document.querySelector("#result");
 
-function run() {
+function runCode() {
   localStorage.setItem("html_code", html_code.value);
   localStorage.setItem("css_code", css_code.value);
   localStorage.setItem("js_code", js_code.value);
@@ -12,6 +12,6 @@ function run() {
     `<style>${localStorage.css_code}</style>` + localStorage.html_code;
   result.contentWindow.eval(localStorage.js_code);
 }
-html_code.onkeyup = () => run();
-css_code.onkeyup = () => run();
-js_code.onkeyup = () => run();
+html_code.onkeyup = () => runCode();
+css_code.onkeyup = () => runCode();
+js_code.onkeyup = () => runCode();
